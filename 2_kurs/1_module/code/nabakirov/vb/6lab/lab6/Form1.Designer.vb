59809110b2659042128e503d3f055e9bb4b5526b
nabakirov <nabakirov@gmail.com>
2017-10-10 07:53:40 +0600

    added asd lab4

---
+++ 2_kurs/1_module/code/nabakirov/vb/6lab/lab6/Form1.Designer.vb
@@ -0,0 +1,149 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.aValue = New System.Windows.Forms.TextBox()
+        Me.bValue = New System.Windows.Forms.TextBox()
+        Me.eValue = New System.Windows.Forms.TextBox()
+        Me.reqValue = New System.Windows.Forms.TextBox()
+        Me.simValue = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.kjjk = New System.Windows.Forms.Label()
+        Me.jhbhj = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'aValue
+        '
+        Me.aValue.Location = New System.Drawing.Point(127, 34)
+        Me.aValue.Name = "aValue"
+        Me.aValue.Size = New System.Drawing.Size(100, 20)
+        Me.aValue.TabIndex = 0
+        '
+        'bValue
+        '
+        Me.bValue.Location = New System.Drawing.Point(127, 78)
+        Me.bValue.Name = "bValue"
+        Me.bValue.Size = New System.Drawing.Size(100, 20)
+        Me.bValue.TabIndex = 1
+        '
+        'eValue
+        '
+        Me.eValue.Location = New System.Drawing.Point(108, 133)
+        Me.eValue.Name = "eValue"
+        Me.eValue.Size = New System.Drawing.Size(100, 20)
+        Me.eValue.TabIndex = 2
+        '
+        'reqValue
+        '
+        Me.reqValue.Location = New System.Drawing.Point(173, 237)
+        Me.reqValue.Name = "reqValue"
+        Me.reqValue.Size = New System.Drawing.Size(100, 20)
+        Me.reqValue.TabIndex = 3
+        '
+        'simValue
+        '
+        Me.simValue.Location = New System.Drawing.Point(173, 185)
+        Me.simValue.Name = "simValue"
+        Me.simValue.Size = New System.Drawing.Size(100, 20)
+        Me.simValue.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(30, 143)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(52, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "точность"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(33, 178)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(107, 33)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Метод Симпсона"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(33, 228)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(107, 37)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Метод прямоугольников"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'kjjk
+        '
+        Me.kjjk.AutoSize = True
+        Me.kjjk.Location = New System.Drawing.Point(93, 41)
+        Me.kjjk.Name = "kjjk"
+        Me.kjjk.Size = New System.Drawing.Size(13, 13)
+        Me.kjjk.TabIndex = 8
+        Me.kjjk.Text = "a"
+        '
+        'jhbhj
+        '
+        Me.jhbhj.AutoSize = True
+        Me.jhbhj.Location = New System.Drawing.Point(93, 85)
+        Me.jhbhj.Name = "jhbhj"
+        Me.jhbhj.Size = New System.Drawing.Size(13, 13)
+        Me.jhbhj.TabIndex = 9
+        Me.jhbhj.Text = "b"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(353, 292)
+        Me.Controls.Add(Me.jhbhj)
+        Me.Controls.Add(Me.kjjk)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.simValue)
+        Me.Controls.Add(Me.reqValue)
+        Me.Controls.Add(Me.eValue)
+        Me.Controls.Add(Me.bValue)
+        Me.Controls.Add(Me.aValue)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents aValue As TextBox
+    Friend WithEvents bValue As TextBox
+    Friend WithEvents eValue As TextBox
+    Friend WithEvents reqValue As TextBox
+    Friend WithEvents simValue As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents kjjk As Label
+    Friend WithEvents jhbhj As Label
+End Class
---
+++ 2_kurs/1_module/code/nabakirov/vb/6lab/lab6/Form1.vb
@@ -0,0 +1,105 @@
+﻿Public Class Form1
+    Dim a As Double
+    Dim b As Double
+    Dim ee As Double
+
+
+    Private Function init()
+        If IsNumeric(aValue.Text) Then
+            a = aValue.Text
+        Else
+            MsgBox("a value is not numeric")
+        End If
+        If IsNumeric(bValue.Text) Then
+            b = bValue.Text
+        Else
+            MsgBox("b value is not numeric")
+        End If
+        If IsNumeric(eValue.Text) Then
+            ee = eValue.Text
+        Else
+            MsgBox("e value is not numeric")
+        End If
+
+    End Function
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles reqValue.TextChanged
+
+    End Sub
+    Function f(ByVal x)
+        Dim ans As Double
+        ans = Math.Sin(x) * (1 + Math.Pow(x, 3))
+        Return ans
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        init()
+        Dim s As Double = 0
+        Dim s0 As Double
+        Dim h As Double
+        Dim x As Double
+        Dim n As Double = 8
+        Do
+            s0 = s
+            h = (b - a) / n
+            s = 0
+            x = a + h / 2
+            Do
+                s = s + f(x) * h
+                x = x + h
+            Loop While x < b
+            n = n * 2
+        Loop While math.Abs(s - s0) > ee
+        reqValue.Text = s
+    End Sub
+
+    Private Function f2(ByVal x)
+        Return 1
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        init()
+        Dim I As Double = 0
+        Dim n As Double = 4
+        Dim h As Double = (b - a) / n
+        Dim I2 As Double
+        Dim j As Double
+        Dim x0 As Double, x1 As Double, x2 As Double
+        Dim y0 As Double, y1 As Double, y2 As Double
+        Dim S As Double
+        Do
+            I2 = 0
+            j = 2
+            Do
+                x2 = a + j * h
+                x1 = x2 - h
+                x0 = x1 - h
+
+                y0 = f(x0)
+                y1 = f(x1)
+                y2 = f(x2)
+                S = y0 + y1 + y2
+                I2 = I2 + S
+                If j < n Then
+                    j = j + 2
+                Else
+                    Exit Do
+                End If
+            Loop
+            I2 = I2 * h / 3
+
+            If Math.Abs(I - I2) < ee Then
+                Exit Do
+            Else
+                I = I2
+                n = n * 2
+                h = h / 2
+            End If
+        Loop
+        simValue.Text = I2
+    End Sub
+End Class
